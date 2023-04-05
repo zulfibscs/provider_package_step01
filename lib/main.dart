b2766f9538ac37259263f87d038b832a2a92a2a4
Zulfi <zulfibscs@gmail.com>
2023-04-05 11:03:57 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_package_step01/provider/count_provider.dart';
+import 'package:provider_package_step01/provider/example_one_provider.dart';
 import 'package:provider_package_step01/screen/count_example.dart';
+import 'package:provider_package_step01/screen/example_one.dart';
+import 'package:provider_package_step01/screen/favourit_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -10,17 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_)=>CounterProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=>CounterProvider(),),
+        ChangeNotifierProvider(create: (_)=>ExampleOneProvider()),
+      ],
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: CountExample(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ExampleOneScreen(),
+    ),
     );
-
   }
 }
 
---
+++ lib/screen/example_one.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/example_one_provider.dart';
+
+class ExampleOneScreen extends StatefulWidget {
+  const ExampleOneScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExampleOneScreen> createState() => _ExampleOneScreenState();
+}
+class _ExampleOneScreenState extends State<ExampleOneScreen> {
+  @override
+
+  Widget build(BuildContext context) {
+    print('object');
+    final provider=Provider.of<ExampleOneProvider>(context,listen: false);
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Consumer<ExampleOneProvider>(builder: (context, value, child) {
+            return Slider(
+                min: 0,
+                max:1 ,
+                value: value.value,
+                onChanged: (val)
+                {
+                  value.setValue(val);
+
+                });
+          },),
+          Consumer<ExampleOneProvider>(builder: (context, value, child) {
+            return  Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.red.withOpacity(provider.value)
+                    ),
+                    child: Center(child: Text('Container 1')),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.green.withOpacity(provider.value)
+                  ),
+                  child: Center(child: Text('Container 1')),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow.withOpacity(provider.value)
+                    ),
+                    child: Center(child: Text('Container 1')),
+                  ),
+                ),
+              ],
+            );
+          },),
+
+        ], 
+      ),
+    );
+  }
+}
+
+
